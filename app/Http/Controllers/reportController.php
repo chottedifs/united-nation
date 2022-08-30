a2b05393f31a8cd80e1c16ff10f01662ebf20068
chottedifs <husadif.aldi@gmail.com>
2022-08-30 20:41:44 +0700

    Update Report View

--- app/Http/Controllers/reportController.php
+++ app/Http/Controllers/reportController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Report;
+use App\Models\Pages;
 use App\Models\RelasiReportPages;
 
 class reportController extends Controller
 {
     public function index(Request $request, $slug)
     {
+        $page = Pages::where('pages_id', 3)->first();
         $report = Report::where('slug',$slug)->firstOrFail();
         $subReports = Report::inRandomOrder()->take(3)->get();
         $reportHuman = RelasiReportPages::with('Pages' , 'Report')->where('pages_id', 3)->get();
@@ -20,6 +22,7 @@ class reportController extends Controller
         // ddd($subReports);
 
         return view('pages.report',[
+            'page' => $page,
             'report' => $report,
             'subReport' => $subReports,
             'reportHuman' => $reportHuman,
